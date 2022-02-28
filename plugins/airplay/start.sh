55cfacd1be8b44796c1925fd486bf2ea2d8a80fa
Tobias Laursen <djerik@gmail.com>
2022-02-28 13:31:04 +0100

    Update start.sh

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -5,7 +5,7 @@ if [[ -n "$SOUND_DISABLE_AIRPLAY" ]]; then
   exit 0
 fi
 
-# --- ENV VARS ---
+# --- ENV VARS ---
 # SOUND_DEVICE_NAME: Set the device broadcast name for AirPlay
 SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound AirPlay $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
 
@@ -14,10 +14,10 @@ echo "Device name: $SOUND_DEVICE_NAME"
 
 # Wait for audioblock to start. This is a bit hacky, but necessary for the time being as
 # shairport-sync will fail silently if audioblock is not ready when it starts up
-# See: https://github.com/mikebrady/shairport-sync/issues/1054
+# See: https://github.com/mikebrady/shairport-sync/issues/1054
 # Remove when above issue is addressed
 SOUND_SUPERVISOR_PORT=${SOUND_SUPERVISOR_PORT:-80}
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):$SOUND_SUPERVISOR_PORT"
+SOUND_SUPERVISOR="http://localhost:$SOUND_SUPERVISOR_PORT"
 while ! curl --silent --output /dev/null "$SOUND_SUPERVISOR/ping"; do sleep 5; echo "Waiting for audioblock to start..."; done
 
 # Start AirPlay
